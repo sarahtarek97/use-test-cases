1b4872feac61216792aa156e74219581a439c3c0
sarahtarek97 <sarahtarek400@gmail.com>
2022-12-08 15:03:17 +0200

    mock

--- db.js
+++ db.js
@@ -1,7 +1,12 @@
 const getOrder = (orderId) => {
-  return { id: orderId, price: 100 };
-};
+  return { id: orderId, price: 100 }
+}
+
+const updateOrder = (order) => {
+  console.log('order is update')
+}
 
 module.exports = {
+  updateOrder,
   getOrder,
-};
+}
--- utils.js
+++ utils.js
@@ -1,44 +1,51 @@
-const db = require("./db");
+const { default: axios } = require('axios')
+const db = require('./db')
 
 //test number
-const sum = (a, b) => a + b;
+const sum = (a, b) => a + b
 
 //test string
-const greeting = (name) => `Hello ${name}!`;
+const greeting = (name) => `Hello ${name}!`
 
 //test boolean
-const isEven = (number) => (number % 2 === 0 ? true : false);
+const isEven = (number) => (number % 2 === 0 ? true : false)
 
 //test arrays
-const ANIMALS = ["cat", "dog", "monkey"];
+const ANIMALS = ['cat', 'dog', 'monkey']
 
 //test objects
 const getOrderById = (id) => {
-  if (!id) throw new Error("id is not defined");
+  if (!id) throw new Error('id is not defined')
   return {
     id: 1,
     price: 10,
-    date: "2022",
-  };
-};
+    date: '2022',
+  }
+}
 
 //test async code
 const getOrders = async () => {
   return [
     { id: 1, price: 10 },
     { id: 2, price: 20 },
-  ];
-};
+  ]
+}
 
 //test mock
 const applyDiscount = (orderId) => {
-  const order = db.getOrder(orderId);
+  const order = db.getOrder(orderId)
 
   if (order.price >= 10) {
-    order.price -= order.price * 0.1;
+    order.price -= order.price * 0.1
+    db.updateOrder(order)
   }
-  return order;
-};
+  return order
+}
+
+const fetchData = async () => {
+  const data = axios.get('https://url.com')
+  return data
+}
 
 module.exports = {
   sum,
@@ -48,4 +55,5 @@ module.exports = {
   getOrderById,
   getOrders,
   applyDiscount,
-};
+  fetchData,
+}
